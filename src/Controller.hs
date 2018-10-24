60afec4ca2125f6acd01db6081ac653efc1b1515
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-10-24 16:02:44 +0200

    ietsje overzichtelijker imo

--- src/Controller.hs
+++ src/Controller.hs
@@ -11,22 +11,15 @@ import System.Random
 --update ja man
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
-step secs gstate
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
-  = -- We show a new random number
-    do randomNumber <- randomIO
-       let newNumber = abs randomNumber `mod` 10
-       return $ GameState (ShowANumber newNumber) 0
-  | otherwise
-  = -- Just update the elapsed time
-    return $ gstate { elapsedTime = elapsedTime gstate + secs }
+step secs gstate | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES = do randomNumber <- randomIO -- We show a new random number
+                   let newNumber = abs randomNumber `mod` 10
+                   return $ GameState (ShowANumber newNumber) 0
+                 | otherwise = return $ gstate { elapsedTime = elapsedTime gstate + secs } -- Just update the elapsed time
 
 -- | Handle user input
 input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) _ _ _) gstate
-  = -- If the user presses a character key, show that one
-    gstate { infoToShow = ShowAChar c }
+inputKey (EventKey (Char c) _ _ _) gstate = gstate { infoToShow = ShowAChar c } -- If the user presses a character key, show that one
 inputKey _ gstate = gstate -- Otherwise keep the same
