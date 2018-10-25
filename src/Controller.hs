c6004e82562438138cac8692b324ed3845a76a40
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-10-25 13:12:59 +0200

    THE ULTIMATE INTERACTION GAME - PRESS W TO START

--- src/Controller.hs
+++ src/Controller.hs
@@ -15,13 +15,13 @@ step secs gstate | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES = -- We sh
                    do randomNumber <- randomIO
                       let newNumber = abs randomNumber `mod` 10
                       return $ GameState (ShowANumber newNumber) 0 (Character 10 20 (Model.Rectangle 20 20)) --deze moet nog anders
-                 | otherwise
-  = return $ gstate { elapsedTime = elapsedTime gstate + secs } -- Just update the elapsed time
+                 | otherwise = return $ gstate { elapsedTime = elapsedTime gstate + secs } -- Just update the elapsed time
 
 -- | Handle user input
 input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) _ _ _) gstate = gstate { infoToShow = ShowAChar c } -- If the user presses a character key, show that one
+inputKey (EventKey (Char c) _ _ _) gstate | c == 'w' = gstate { character = (character gstate) { xp = 100 } } -- If the user presses a character key, show that one
+                                          | otherwise = gstate { infoToShow = ShowAChar c }
 inputKey _ gstate = gstate -- Otherwise keep the same
